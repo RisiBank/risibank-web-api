59797cfcbf5649acfe37098bf96a4d4331bf90c3
7PH <b.raymond@protonmail.com>
2024-07-08 01:16:44 +0200

    Fix exiting RisiBank modal

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    static RISIBANK_URL = 'https://risibank.fr/';
+    static RISIBANK_URL = 'https://risibank.fr';
 
     static RISIBANK_CACHE_REGEX =
         /https:\/\/risibank.fr\/cache\/medias\/(\d+)\/(\d+)\/(\d+)\/(\d+)\/(\w+)\.(jpg|jpeg|gif|png)/;
